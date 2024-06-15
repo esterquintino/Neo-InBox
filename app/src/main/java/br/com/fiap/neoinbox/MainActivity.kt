3292a75bfb5ced5f3d1aa6b2005a7e268f314b9b
esterquintino <esterqdasilva@outlook.com>
2024-06-15 13:09:21 -0300

    Criação de packages e configuração do Strings.xml

---
+++ app/src/main/java/br/com/fiap/neoinbox/MainActivity.kt
@@ -0,0 +1,60 @@
+
+
+
+package br.com.fiap.neoinbox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.neoinbox.ui.theme.NeoInboxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NeoInboxTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Loading()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Loading() {
+   Column {
+       Row {
+           Text(
+               text = "Hello User!",
+           )
+       }
+       Row {
+           Button(onClick = { /*TODO*/ } ) {
+               Text(text = "Seguir ->")
+           }
+       }
+   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingPreview() {
+    NeoInboxTheme {
+        Loading()
+    }
+}
