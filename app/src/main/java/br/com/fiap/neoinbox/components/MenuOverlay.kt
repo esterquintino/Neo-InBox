ce66a1eb9f371034cd5802d5b2340f893e8cb38c
esterquintino <esterqdasilva@outlook.com>
2024-06-17 04:21:22 -0300

    Correção de bugs no gradle

--- app/src/main/java/br/com/fiap/neoinbox/components/MenuOverlay.kt
+++ app/src/main/java/br/com/fiap/neoinbox/components/MenuOverlay.kt
@@ -13,12 +13,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.fiap.neoinbox.R
@@ -29,37 +31,44 @@ fun MenuOverlay(onDismiss: () -> Unit) {
         modifier = Modifier
             .fillMaxHeight()
             .width(300.dp)
-            .background(colorResource(id = R.color.cinzaescuro)),
+            .background(Color(0xFF2F2F2F)),
         contentAlignment = Alignment.TopStart
     ) {
         Column(
             modifier = Modifier
-                .background(colorResource(id = R.color.cinzaescuro))
+                .background(Color(0xFF2F2F2F))
                 .padding(20.dp)
                 .fillMaxSize(),
             verticalArrangement = Arrangement.Top
         ) {
-            Row (
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.LightGray)
-            ){
-                Text(
-                    text = "Tipo de conta",
+                    .height(150.dp)
+            ) {
+                Column() {
+                    Text(
+                        text = "Tipo de conta",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 10.dp)
+                    )
+                    Text(
+                        text = "endereço de email",
+                        modifier = Modifier
+                            .clickable { onDismiss() }
+                            .fillMaxWidth()
+                    )
+                }
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_user_profile),
+                    contentDescription = "Perfil de usuário",
                     modifier = Modifier
+                        .height(40.dp)
+                        .width(40.dp)
                         .clickable { onDismiss() }
-                        .fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-
-                Text(
-                    text = "endereço de email",
-                    modifier = Modifier
-                        .clickable { onDismiss() }
-                        .fillMaxWidth()
                 )
             }
-
         }
     }
 }
