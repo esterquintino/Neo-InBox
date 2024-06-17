5b51835e020af767bcfe02914d53b876f3999f4c
esterquintino <esterqdasilva@outlook.com>
2024-06-17 18:35:14 -0300

    Dao e repository

--- app/src/main/java/br/com/fiap/neoinbox/database/dao/EventoDao.kt
+++ app/src/main/java/br/com/fiap/neoinbox/database/dao/EventoDao.kt
@@ -1,7 +1,25 @@
 package br.com.fiap.neoinbox.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import br.com.fiap.neoinbox.model.Conta
+import br.com.fiap.neoinbox.model.Evento
 
 @Dao
 interface EventoDao {
+    @Insert
+    fun salvarEvento(evento: Evento): Long
+
+    @Update
+    fun atualizarEvento(evento: Evento): Int
+
+    @Delete
+    fun excluirEvento(evento: Evento): Int
+
+    @Query("SELECT * FROM tbl_evento WHERE cod_conta_fk = :codContaFK")
+    fun listarEventos(codContaFK: Long): List<Evento>
+
 }
--- app/src/main/java/br/com/fiap/neoinbox/database/repository/EventoRepository.kt
+++ app/src/main/java/br/com/fiap/neoinbox/database/repository/EventoRepository.kt
@@ -1,4 +1,24 @@
 package br.com.fiap.neoinbox.database.repository
 
-class EventoRepository {
+import android.content.Context
+import br.com.fiap.neoinbox.database.dao.NeoinboxDb
+import br.com.fiap.neoinbox.model.Conta
+import br.com.fiap.neoinbox.model.Evento
+
+class EventoRepository(context: Context) {
+    var db = NeoinboxDb.getDatabase(context).eventoDao()
+
+    fun salvarEvento(evento: Evento): Long{
+        return db.salvarEvento(evento = evento)
+    }
+
+    fun atualizarEvento(evento: Evento): Int{
+        return db.atualizarEvento(evento = evento)
+    }
+    fun excluirEvento(evento: Evento): Int{
+        return db.excluirEvento(evento = evento)
+    }
+    fun listarEventos(codContaFK: Long): List<Evento>{
+        return db.listarEventos(codContaFK)
+    }
 }
