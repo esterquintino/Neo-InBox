3046b04b0d2421aa846151d283ce6eda7efd9154
esterquintino <esterqdasilva@outlook.com>
2024-06-17 22:00:55 -0300

    Ajuste design CalendarioScreen

--- app/src/main/java/br/com/fiap/neoinbox/calendario/CalendarioScreen.kt
+++ app/src/main/java/br/com/fiap/neoinbox/calendario/CalendarioScreen.kt
@@ -11,9 +11,17 @@ import br.com.fiap.neoinbox.R
 import java.util.Calendar
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -24,8 +32,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import br.com.fiap.neoinbox.cadastro.CadastroScreenViewModel
+import br.com.fiap.neoinbox.components.Botao
+import br.com.fiap.neoinbox.components.BotaoMenu
+import br.com.fiap.neoinbox.components.CaixaDeEntrada
+import br.com.fiap.neoinbox.components.EmailCard
+import br.com.fiap.neoinbox.components.EmailContent
+import br.com.fiap.neoinbox.components.IconeUsuario
 import java.util.Date
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -43,17 +65,45 @@ fun CalendarioScreen(
     val currentMonth = calendar.get(Calendar.MONTH)
     val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "Calendario") },
-            )
-        },
-        content = {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
+
+    Box(
+        modifier = Modifier
+            .background(colorResource(id = R.color.areia)),
+        contentAlignment = Alignment.TopStart
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Top
+            ) {
+                BotaoMenu(navController)
+                Spacer(modifier = Modifier.weight(1f))
+                IconeUsuario(
+                    modifier = Modifier,
+                )
+            }
+            Spacer(modifier = Modifier.height(48.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.Top
+            ) {
+                Text(
+                    text = "Calendário",
+                    style = TextStyle(
+                        fontFamily = FontFamily(Font(R.font.inter_semibold)),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 32.sp,
+                        color = (colorResource(id = R.color.cinzaescuro))
+                    )
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.Top
             ) {
                 AndroidView(factory = { CalendarView(it) }, update = {
                     it.date = calendar.timeInMillis
@@ -63,10 +113,24 @@ fun CalendarioScreen(
                 })
                 Text(text = date)
             }
-
         }
-    )
+    }
+
 }
+
+//Column(
+//verticalArrangement = Arrangement.Center,
+//horizontalAlignment = Alignment.CenterHorizontally,
+//modifier = Modifier.fillMaxSize()
+//) {
+//    AndroidView(factory = { CalendarView(it) }, update = {
+//        it.date = calendar.timeInMillis
+//        it.setOnDateChangeListener { calendarView, year, month, day ->
+//            date = "$day - ${month + 1} - $year"
+//        }
+//    })
+//    Text(text = date)
+//}
 
 //@Preview
 //@Composable
